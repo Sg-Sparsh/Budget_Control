77bbb29019330b483734582c5cb7267d03daee7c
Sanskar Gupta <sanskarsg22@outlook.com>
2020-04-30 20:30:55 +0530

    second commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer">
+        <center>
+            <p>Copyright © Control Budget. All Rights Reserved|Contact Us: +91-8190847372</p>
+        </center>
+    </div>
+</footer>
